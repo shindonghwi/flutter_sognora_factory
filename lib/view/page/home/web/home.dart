39abcfb089fb93f879ade1d438fc6c0c372dd449
shindonghwi <ehdgnl8940@naver.com>
2022-10-11 18:49:15 +0900

    fix: scaffold appbar remove

--- lib/view/page/home/web/home.dart
+++ lib/view/page/home/web/home.dart
@@ -12,22 +12,25 @@ class _WebPageHomeState extends State<WebPageHome> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('AppBar'),
-        actions: [SwitchThemeToggleButton()],
-      ),
       body: LayoutBuilder(
         builder: (BuildContext context, BoxConstraints viewportConstraints) {
           return Column(children: [
             SingleChildScrollView(
               child: ConstrainedBox(
                 constraints: BoxConstraints(
-                  minHeight: 1.0,
+                  minHeight: viewportConstraints.maxHeight,
                   minWidth: viewportConstraints.maxWidth,
                 ),
                 child: Column(
                   children: [
-                    Text(style: TextStyle(fontSize: 35),'AppBar'),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [Text("111 : "), Text("222 : ")],
+                    ),
+                    Text(
+                      style: TextStyle(fontSize: 35),
+                      'AppBar',
+                    ),
                   ],
                 ),
               ),
