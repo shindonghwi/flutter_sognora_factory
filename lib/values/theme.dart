50dce83b6d4c3f94afc4680397a617a7aa7ba978
shindonghwi <ehdgnl8940@naver.com>
2022-10-11 18:02:48 +0900

    fix: dark primary color change

--- lib/values/theme.dart
+++ lib/values/theme.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 
 final darkTheme = ThemeData(
-  primaryColor: Colors.black,
+  primaryColor: const Color.fromARGB(255, 33, 31, 59),
   brightness: Brightness.dark,
   dividerColor: Colors.black12,
+
 );
 
 final lightTheme = ThemeData(
